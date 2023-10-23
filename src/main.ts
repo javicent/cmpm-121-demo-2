fdcdf34ebe093b4a3765f7aaae64b89445b3cef1
Justin Vicente <javicent@ucsc.edu>
2023-10-23 13:49:56 -0700

    Step 1

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "JSTN's game";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+
+canvas.classList.add("canvas-style");
+
+app.appendChild(canvas);
