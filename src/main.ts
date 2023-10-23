97a50262c43b8c33108c1ee2ad059df5f1378c3c
Justin Vicente <javicent@ucsc.edu>
2023-10-23 13:51:43 -0700

    Step 2

--- src/main.ts
+++ src/main.ts
@@ -17,3 +17,37 @@ canvas.height = 256;
 canvas.classList.add("canvas-style");
 
 app.appendChild(canvas);
+
+const ctx = canvas.getContext("2d");
+
+let isDrawing = false;
+
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  ctx.beginPath();
+  ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (!isDrawing) return;
+  ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+  ctx.stroke();
+});
+
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+  ctx.closePath();
+});
+
+canvas.addEventListener("mouseout", () => {
+  isDrawing = false;
+  ctx.closePath();
+});
+
+const clearButton = document.createElement("button");
+clearButton.textContent = "Clear";
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+app.appendChild(clearButton);
